5b366501e0cfa48f6b478c0e43dda9d0364bff9b
Uladzislau Kastsevich <uladzislau_kastsevich@epam.com>
2018-05-20 07:45:30 +0300

    request

--- app/components/requestList.component.js
+++ app/components/requestList.component.js
@@ -7,6 +7,28 @@ function RequestListController(requestService, $rootScope) {
     var $ctrl = this;
 
     $ctrl.$onInit = function () {
+        setRequestList();
+    };
+
+    $ctrl.isAdmin = function () {
+        return $rootScope.sessionUserRole == 0;
+    };
+    
+    $ctrl.approveRequest = function (request) {
+        request.status = 2;
+        requestService.updateRequest(request).then(function (request) {
+            setRequestList();
+        });
+    };
+
+    $ctrl.rejectRequest = function (request) {
+        request.status = 1;
+        requestService.updateRequest(request).then(function (request) {
+            setRequestList();
+        });
+    };
+    
+    function setRequestList() {
         if ($rootScope.sessionUserRole == 0) {
             requestService.getAllRequests().then(function (requestList) {
                 $ctrl.requestList = requestList;
@@ -16,5 +38,5 @@ function RequestListController(requestService, $rootScope) {
                 $ctrl.requestList = requestList;
             });
         }
-    };
+    }
 }
--- app/filters/requestStatus.filter.js
+++ app/filters/requestStatus.filter.js
@@ -3,6 +3,8 @@ function requestStatusFilter() {
         if (status == 0) {
             return "На рассмотрении";
         } else if (status == 1) {
+            return "Оклонена";
+        } else if (status == 2) {
             return "Одобрена";
         } else {
             return "";
